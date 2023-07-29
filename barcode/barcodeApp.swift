27784bb72cdaa48c2abee719f04bad930f1331ac
iqbal <ifachry@rocketmail.com>
2023-07-29 23:37:12 +0700

    Initial Commit

---
+++ barcode/barcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  barcodeApp.swift
+//  barcode
+//
+//  Created by Iqbal Fachry on 29/07/23.
+//
+
+import SwiftUI
+
+@main
+struct barcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
